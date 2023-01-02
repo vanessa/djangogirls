c0709c2b25c2d36c25b5c11eeecb397678c656af
Vanessa Barreiros <vanessa@monarchmoney.com>
2023-01-02 03:09:35 -0300

    Reset JS files

--- static/source/js/applications.js
+++ static/source/js/applications.js
@@ -1,101 +1,90 @@
-import $ from "jquery";
-
-$(document).ready(function () {
-  $("#select-all-box").click(function () {
-    $(".application-box").prop("checked", $(this).prop("checked"));
-  });
-
-  $(".application-box, #select-all-box").click(function () {
-    updateSelectedCount();
-  });
-
-  $(".state-change").click(function (e) {
-    var state = $(this).data("state"),
-      appId = $(this).data("app-id"),
-      url = $("#applications").data("change-state-url"),
-      name = $(this).text();
-
-    $.post(url, { state: state, application: appId }, function (data) {
-      updateApplicationState(appId, state, name);
+$(document).ready(function() {
+    $("#select-all-box").click(function () {
+        $(".application-box").prop('checked', $(this).prop('checked'));
     });
-    e.preventDefault();
-  });
 
-  $("#change-state-form").submit(function () {
-    var url = $(this).attr("action"),
-      state = $($(this).find("option:selected")).val(),
-      name = $($(this).find("option:selected")).text();
+    $('.application-box, #select-all-box').click(function() {
+        updateSelectedCount();
+    });
 
-    $.post(url, $(this).serialize(), function (data) {
-      if (data["updated"]) {
-        data["updated"].forEach(function (id) {
-          updateApplicationState(id, state, name);
-          $("#application-" + id + "-box").prop("checked", false);
+    $('.state-change').click(function(e){
+        var state = $(this).data('state'),
+            appId = $(this).data('app-id'),
+            url = $('#applications').data('change-state-url'),
+            name = $(this).text();
+
+        $.post(url, {'state': state, 'application': appId}, function(data){
+            updateApplicationState(appId, state, name);
         });
-      }
-      updateSelectedCount();
+        e.preventDefault();
     });
 
-    return false;
-  });
+    $('#change-state-form').submit(function(){
+        var url = $(this).attr('action'),
+            state = $($(this).find('option:selected')).val(),
+            name = $($(this).find('option:selected')).text();
 
-  $(".rsvp-change").click(function () {
-    var rsvp_status = $(this).data("rsvp"),
-      appId = $(this).data("app-id"),
-      url = $("#applications").data("change-rsvp-url"),
-      name = $(this).text();
+        $.post(url, $(this).serialize(), function(data){
+            if (data['updated']){
+                data['updated'].forEach(function(id){
+                    updateApplicationState(id, state, name);
+                    $('#application-'+id+'-box').prop('checked', false);
+                });
+            }
+	    updateSelectedCount();
+        });
 
-    $.post(
-      url,
-      { rsvp_status: rsvp_status, application: appId },
-      function (data) {
-        updateApplicationRsvp(appId, rsvp_status, name);
-      }
-    );
-  });
+        return false;
+    });
 
-  $("#id_text").keyup(function () {
-    updatePreview($(this).val());
-  });
+    $('.rsvp-change').click(function(){
+        var rsvp_status = $(this).data('rsvp'),
+            appId = $(this).data('app-id'),
+            url = $('#applications').data('change-rsvp-url'),
+            name = $(this).text();
 
-  $(".rsvp-buttons .btn").click(function () {
-    var option = $(this).data("option");
-    if (option == "yes" || option == "no") {
-      var rsvp_link = "\n[rsvp-url-" + option + "]";
-      var message = $("#id_text").val() + rsvp_link;
-      $("#id_text").val(message);
-      updatePreview(message);
+        $.post(url, {'rsvp_status': rsvp_status, 'application': appId}, function(data){
+            updateApplicationRsvp(appId, rsvp_status, name);
+        });
+    });
+
+    $('#id_text').keyup(function(){
+        updatePreview($(this).val());
+    });
+
+    $('.rsvp-buttons .btn').click(function(){
+        var option = $(this).data('option');
+        if (option == 'yes' || option == 'no'){
+            var rsvp_link = '\n[rsvp-url-'+option+']';
+            var message = $('#id_text').val()+rsvp_link;
+            $('#id_text').val(message);
+            updatePreview(message);
+        }
+    });
+
+    function updatePreview(value) {
+        var message = value;
+        message = message.replace(/\[rsvp-url-yes\]/g, '<a href="">http://djangogirls.org/rsvp-YES-generated-url</a>');
+        message = message.replace(/\[rsvp-url-no\]/g, '<a href="">http://djangogirls.org/rsvp-NO-generated-url</a>');
+        $('#preview').html(message);
     }
-  });
 
-  function updatePreview(value) {
-    var message = value;
-    message = message.replace(
-      /\[rsvp-url-yes\]/g,
-      '<a href="">http://djangogirls.org/rsvp-YES-generated-url</a>'
-    );
-    message = message.replace(
-      /\[rsvp-url-no\]/g,
-      '<a href="">http://djangogirls.org/rsvp-NO-generated-url</a>'
-    );
-    $("#preview").html(message);
-  }
+    function updateApplicationState(id, state, stateName){
+        $('#application-'+id+'-state')
+            .removeClass('submitted accepted rejected default waitlisted')
+            .addClass(state)
+            .html(stateName+' <span class="caret"></span>');
+    }
 
-  function updateApplicationState(id, state, stateName) {
-    $("#application-" + id + "-state")
-      .removeClass("submitted accepted rejected default waitlisted")
-      .addClass(state)
-      .html(stateName + ' <span class="caret"></span>');
-  }
+    function updateApplicationRsvp(id, rsvp, rsvpName){
+        $('#application-'+id+'-rsvp-status')
+            .removeClass('waiting yes no')
+            .addClass(rsvp)
+            .html(rsvpName+' <span class="caret"></span>');
+    }
 
-  function updateApplicationRsvp(id, rsvp, rsvpName) {
-    $("#application-" + id + "-rsvp-status")
-      .removeClass("waiting yes no")
-      .addClass(rsvp)
-      .html(rsvpName + ' <span class="caret"></span>');
-  }
+    function updateSelectedCount(){
+        $('#selected-count').html($('.application-box:checked').length);
+    }
 
-  function updateSelectedCount() {
-    $("#selected-count").html($(".application-box:checked").length);
-  }
 });
--- static/source/js/global.js
+++ static/source/js/global.js
@@ -1,24 +1,27 @@
-import $ from "jquery";
+$(document).ready(function() {
 
-$(document).ready(function () {
-  function imitate_link(element) {
-    var url = $(element).attr("rel");
-    if (url != undefined) window.location = url;
-  }
+    $('.gallery').sss({
+        showNav: false
+    });
 
-  $(".event .credit").click(function () {
-    imitate_link(this);
-    return false;
-  });
+    function imitate_link(element) {
+        var url = $(element).attr('rel');
+        if (url != undefined) window.location = url;
+    }
 
-  $(".event .credit span").click(function () {
-    imitate_link(this);
-    return false;
-  });
+    $('.event .credit').click(function() {
+        imitate_link(this);
+        return false;
+    });
 
-  $(".show-more a").click(function () {
-    $(".upcoming-events .row-container").css("max-height", "100%");
-    $(".upcoming-events .row-container").css("overflow", "visible");
-    $(".show-more").css("display", "none");
-  });
+    $('.event .credit span').click(function() {
+        imitate_link(this);
+        return false;
+    });
+
+    $('.show-more a').click(function(){
+        $('.upcoming-events .row-container').css('max-height', '100%');
+        $('.upcoming-events .row-container').css('overflow', 'visible');
+        $('.show-more').css('display', 'none');
+    });
 });
